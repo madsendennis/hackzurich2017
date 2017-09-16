1ad5d4aaa901604a4e64b07a438067d4026a5dfe
rufus peter lobo <rufuslobo@rufuss-MacBook-Pro.local>
2017-09-16 04:12:03 +0200

    capture image REST API - bug fix

--- camera/captureImage.py
+++ camera/captureImage.py
@@ -2,10 +2,11 @@ import picamera
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/captureImage")
+@app.route("/")
 def captureImage():
     camera = picamera.PiCamera()
     camera.capture('cameraImages/Image.jpg')
+    return "test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
