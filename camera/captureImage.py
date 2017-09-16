97d0577851ff7be1dd49a6de51197da23d261ff2
rufus peter lobo <rufuslobo@rufuss-MacBook-Pro.local>
2017-09-16 04:06:44 +0200

    Capture Image using REST interface

--- camera/captureImage.py
+++ camera/captureImage.py
@@ -1,9 +1,11 @@
 import picamera
+from flask import Flask
+app = Flask(__name__)
 
-# Capture Image to file 
+@app.route("/captureImage")
 def captureImage():
     camera = picamera.PiCamera()
     camera.capture('cameraImages/Image.jpg')
 
 if __name__ == "__main__":
-    captureImage()
+    app.run(host='0.0.0.0', port=80, debug=True)
