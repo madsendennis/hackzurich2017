42575c8aa9c39dd360b7d325a9220b99f28fc28e
rufus peter lobo <rufuslobo@rufuss-MacBook-Pro.local>
2017-09-16 11:07:08 +0200

    global variable for picamera

--- camera/captureImage.py
+++ camera/captureImage.py
@@ -4,14 +4,15 @@ import time
 from flask import Flask
 
 app = Flask(__name__)
+camera = picamera.PiCamera()
 
 @app.route("/captureImage")
 def captureImage():
     # capture image from camera
-    self.camera.start_preview()
-    time.sleep(2)
-    self.camera.capture('cameraImages/Image.jpg')
-    self.camera.stop_preview()
+    camera.start_preview()
+    sleep(2)
+    camera.capture('cameraImages/Image.jpg')
+    camera.stop_preview()
 
     # convert image to base64 string
     with open('cameraImages/Image.jpg', 'rb') as imageFile:
@@ -22,4 +23,3 @@ def captureImage():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-    self.camera = picamera.PiCamera()
