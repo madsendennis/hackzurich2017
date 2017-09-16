d748f3fbd553b48e32d6c0259b9ac04b19a4c4e1
rufus peter lobo <rufuslobo@rufuss-MacBook-Pro.local>
2017-09-16 02:51:06 +0200

    capture image basic code

---
+++ camera/captureImage.py
@@ -0,0 +1,9 @@
+import picamera
+
+# Capture Image to file 
+def captureImage():
+	camera = picamera.PiCamera()
+    camera.capture('cameraImages/Image.jpg')
+
+if __name__ == "__main__":
+    captureImage()
