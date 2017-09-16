bf0c9e1d33bea8c288f8d8b89fb74101f82ece88
rufus peter lobo <rufuslobo@rufuss-MacBook-Pro.local>
2017-09-16 13:03:19 +0200

    send image to magic box

--- webApp/js/dashboard.js
+++ webApp/js/dashboard.js
@@ -1,20 +1,43 @@
 $(document).ready(function () {
     var getCameraImageUrl = "http://172.30.0.63:5000/captureImage";
-    
-    $("#getGrainImage").show();
-    $("#contaminationReport").hide();
+    var sendImageToMagicBoxUrl = "http://localhost:5000/receiveImage";
+    var imageBase64String = "";
 
+    $("#takeImage").show();
+    $("#contaminationReport").hide();
+    $("#getContaminationReport").hide();
+
+    // Get Grain Image content
     $("#getGrainImage").click(function () {
         alert("Getting Image!");
         $.ajax({
             url: getCameraImageUrl,
             success: function (data) {
-
+                // Send grain image content to Magic Box
+                imageBase64String = data;
+                $("#getContaminationReport").show();
             },
             error: function () {
 
             },
             async: false
-          });
+        });
+    });
+    
+    $("#getContaminationReport").click(function () {
+        $.ajax({
+            url: sendImageToMagicBoxUrl,
+            type: "POST",
+            data: {
+                imageString: imageBase64String
+            },
+            success: function (data) {
+                $("#contaminationReport").show();                
+            },
+            error: function () {
+
+            },
+            async: false
+        });
     });
 });
